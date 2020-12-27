67ce18e753c8d097586fda929dc6335a4beba715
Tea Cup <lw0482352@gmail.com>
2020-12-26 23:28:00 -0500

    Create me.js

---
+++ src/pages/me.js
@@ -0,0 +1,56 @@
+import React, {
+    Component
+}
+from 'react';
+import api from '../meta/api';
+
+class Me extends Component {
+    state = {
+        code: null,
+        user: null,
+        loaded: false,
+        servers: []
+    };
+
+    async componentDidMount() {
+        let code = localStorage.getItem('code');
+        if (code === "n/a") return window.location.href = api.getOauth()
+        if (!code) return window.location.href = api.getOauth()
+
+        let info = await api.servers(localStorage.getItem('user'));
+        console.log(info)
+        this.state({
+            isLoaded: true,
+            servers: info.servers
+        });
+    }
+
+    render() {
+        const {
+            error, isLoaded, servers
+        } = this.state;
+        console.log(servers)
+
+        if (!isLoaded) {
+            return ( < div >
+                < Navbar / >
+                < center > < br / > < br / > < br / > < br / >
+                < Spinner animation = "border"
+                role = "status" >
+                < span className = "sr-only" > Loading... < /span>  < /Spinner> <h3> Please wait while we load your info </h
+                3 >
+                < br / >
+                < small > If this does not stop spinning then something happened... < /small> < /center> < Footer / >
+                < /div>
+            )
+        } else {
+
+            return ( < div > { /* you can change this your self to look better so that its not just the server names :) */ } {
+                    servers.map(guild => guild.name
+                    } < /div>
+                )
+            }
+        }
+    }
+
+    export default (Me);
